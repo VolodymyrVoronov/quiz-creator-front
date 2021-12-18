fd18eda0e990e357184d1e881e970a46f07ba225
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-12-18 19:46:20 +0100

    chore(StartPage): added animation for buttons and title.

--- src/components/StartPageButtons/StartPageButtons.tsx
+++ src/components/StartPageButtons/StartPageButtons.tsx
@@ -1,40 +1,76 @@
 import { FC } from "react";
 import { motion } from "framer-motion";
+import { ImPencil, ImList2 } from "react-icons/im";
 
 import styles from "./StartPageButtons.module.css";
 
 interface IStartPageButtonsProps {
-  onClickTopButton?: () => void;
-  topButtonText?: string;
-  topButtonArialLabel?: string;
+  onTopButtonClick: () => void;
+  topButtonText: string;
+  topButtonArialLabel: string;
+  topButtonDisabled?: boolean;
 
-  onClickBottomButton?: () => void;
-  bottomButtonText?: string;
-  bottomButtonArialLabel?: string;
+  onBottomButtonClick: () => void;
+  bottomButtonText: string;
+  bottomButtonArialLabel: string;
+  bottomButtonDisabled?: boolean;
 }
 
 const StartPageButtons: FC<IStartPageButtonsProps> = ({
-  onClickTopButton,
-  onClickBottomButton,
+  onTopButtonClick,
+  onBottomButtonClick,
   topButtonArialLabel,
   bottomButtonArialLabel,
   topButtonText,
   bottomButtonText,
+  topButtonDisabled,
+  bottomButtonDisabled,
 }): JSX.Element => {
   return (
-    <div className={styles.buttonsContainer}>
-      <button className={`${styles.button} ${styles.topButton}`} type="button">
-        Create new Quiz
-        <span>😀</span>
-      </button>
-      <button
+    <motion.div
+      animate={{ rotate: 360 }}
+      transition={{ delay: 3.1, duration: 1, ease: "easeIn" }}
+      className={styles.buttonsContainer}
+    >
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ x: [-550, 100, 0], opacity: 1 }}
+        transition={{ duration: 3 }}
+        className={`${styles.button} ${styles.topButton}`}
+        type="button"
+        onClick={onTopButtonClick}
+        aria-label={topButtonArialLabel}
+        disabled={topButtonDisabled}
+      >
+        {topButtonText}
+        <motion.span
+          animate={{ rotate: 360 }}
+          transition={{ delay: 4.1, duration: 0.5, ease: "easeIn" }}
+          className={styles.icon}
+        >
+          <ImPencil />
+        </motion.span>
+      </motion.button>
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ x: [550, -100, 0], opacity: 1 }}
+        transition={{ duration: 3 }}
         className={`${styles.button} ${styles.bottomButton}`}
         type="button"
+        onClick={onBottomButtonClick}
+        aria-label={bottomButtonArialLabel}
+        disabled={bottomButtonDisabled}
       >
-        <span>😀</span>
-        All quizzes
-      </button>
-    </div>
+        <motion.span
+          animate={{ rotate: 360 }}
+          transition={{ delay: 4.1, duration: 0.5, ease: "easeIn" }}
+          className={styles.icon}
+        >
+          <ImList2 />
+        </motion.span>
+        {bottomButtonText}
+      </motion.button>
+    </motion.div>
   );
 };
 
--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import { motion } from "framer-motion";
 import { Flex, Box, Text } from "bumbag";
 
 import StartPageButtons from "components/StartPageButtons/StartPageButtons";
 
 const StartPage: FC<{}> = (): JSX.Element => {
+  const onCreateNewQuizButtonClick = () => {};
+
+  const onAllQuizzesButtonClick = () => {};
+
   return (
     <Flex
       justifyContent="center"
@@ -12,12 +17,25 @@ const StartPage: FC<{}> = (): JSX.Element => {
       height="100vh"
     >
       <Box>
-        <Text fontSize="56px" fontWeight="bold">
-          Quiz Creator
-        </Text>
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 2, delay: 5.6 }}
+        >
+          <Text fontSize="56px" fontWeight="bold">
+            Quiz Creator
+          </Text>
+        </motion.span>
       </Box>
       <Box marginTop="25px">
-        <StartPageButtons />
+        <StartPageButtons
+          onTopButtonClick={onCreateNewQuizButtonClick}
+          topButtonText="Create new quiz"
+          topButtonArialLabel="Create new quiz button"
+          onBottomButtonClick={onAllQuizzesButtonClick}
+          bottomButtonText="All quizzes"
+          bottomButtonArialLabel="All quizzes button"
+        />
       </Box>
     </Flex>
   );
